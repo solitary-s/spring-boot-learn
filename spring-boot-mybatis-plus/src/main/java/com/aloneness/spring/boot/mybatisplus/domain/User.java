19c1b3267fb23ac07aa4ae47b621981eec409e49
aloneness <logic9711@foxmail.com>
2020-07-09 16:00:54 +0800

    mybatis plus

--- spring-boot-mybatis-plus/src/main/java/com/aloneness/spring/boot/mybatisplus/domain/User.java
+++ spring-boot-mybatis-plus/src/main/java/com/aloneness/spring/boot/mybatisplus/domain/User.java
@@ -1,17 +1,22 @@
 package com.aloneness.spring.boot.mybatisplus.domain;
 
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 /**
  * @author aloneness
  * @date 2020/7/3
  */
-@Data
+@Setter
+@Getter
+@ToString
 public class User {
 
     private Long id;
 
-    private String name;
+    private String userName;
 
     private Integer age;
 
